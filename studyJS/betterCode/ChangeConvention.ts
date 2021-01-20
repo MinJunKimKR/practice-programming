b8980037fef2b0dc599dad93124b27c9341137c2
minjunKim <rlaalswns321@naver.com>
2021-01-20 15:59:11 +0900

    feat : jest test code 공부 시작

--- studyJS/betterCode/ChangeConvention.ts
+++ studyJS/betterCode/ChangeConvention.ts
@@ -1,13 +1,31 @@
 // 1 Object의 key값을 snake case로 바꾸고 싶을떄
 
 import has from "has";
-import { snakeCase } from "change-case";
+import { snakeCase, camelCase } from "change-case";
 const original: { [key: string]: any } = {
   helloWorld: "hihi",
 };
+const originalSnake: { [key: string]: any } = {
+  hello_world: "hihi",
+  hi_there: "hi there",
+};
+
+const insertObject = {
+  helloWorld: originalSnake.hello_world,
+  hiThere: originalSnake.hi_there,
+};
+// console.log(originalSnake);
+// console.log(insertObject);
 //{ [key :string]}을 적어주는 이유는
 //key : string을 해주면 key값으로 'key'라는 string 만사용이 가능함
 //하지만 우리는 여러가지 이름의 key값을 사용하고 싶음으로 []로 감싸고 :any type을 넣어준다
+const converted: { [key: string]: any } = {};
+for (const prop in originalSnake) {
+  if (has(originalSnake, prop)) {
+    converted[snakeCase(prop)] = originalSnake[prop];
+  }
+}
+console.log(converted);
 
 /*
 
@@ -29,12 +47,13 @@ const original: { [key: string]: any } = {
 로 해결
  * 
  */
-const converted: { [key: string]: any } = {};
-for (const prop in original) {
-  if (has(original, prop)) {
-    //prop이 property로 존재하는지 확정하기 위해서 사용
-    converted[snakeCase(prop)] = original[prop]; //[]를 사용해서 string 겂을 key로 넣어준다
-    // ex. converted['hello_world'] = original['helloWorld']
-  }
-}
-console.log(converted);
+
+// const converted: { [key: string]: any } = {};
+// for (const prop in original) {
+//   if (has(original, prop)) {
+//     //prop이 property로 존재하는지 확정하기 위해서 사용
+//     converted[snakeCase(prop)] = original[prop]; //[]를 사용해서 string 겂을 key로 넣어준다
+//     // ex. converted['hello_world'] = original['helloWorld']
+//   }
+// }
+// console.log(converted);
