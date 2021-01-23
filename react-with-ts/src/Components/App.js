6cf296a240eb4bbedeb5610ac156dcd20993c841
minjunkim <rlaalswns321@naver.com>
2021-01-23 16:25:57 +0900

    feat : 2강 완료

--- react-with-ts/src/Components/App.js
+++ react-with-ts/src/Components/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
-import Router from "Components/Router";
+import React, { Component } from 'react';
+import Router from 'Components/Router';
+import Header from 'Components/Header';
+//민들어둔 헤더를 import 하면 router 밖에 있기 때문에 항사 노출이 된다
 //여러개의 리턴을 위하여 flagment를 쓴다
 class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <Router />
       </>
     );
