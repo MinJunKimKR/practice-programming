b279aec7192b8d14f66663bb85179bebe7a2316b
minjunkim <rlaalswns321@naver.com>
2021-03-27 18:23:31 +0900

    feat: url

--- algorithm/programmers_level2_funtionDevelop.js
+++ algorithm/programmers_level2_funtionDevelop.js
@@ -1,5 +1,8 @@
 // preogresses : 작업도가 적힌 배열
 // speeds : 각 progresses 배열의 작업속도
+// 배포는 앞의 작업이 배포가 될때 이루어진다
+// 뒤에서 부터 서칭한다음에, 100 이상의 processes가 채워진 배열을
+// refer :https://programmers.co.kr/learn/courses/30/lessons/42586?language=javascript
 
 function solution(progresses, speeds) {
   var answer = [];
