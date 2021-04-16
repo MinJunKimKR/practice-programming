2d9ff83dfc2b531749cc80b6072b81cf27f9fa4e
minjunKim <rlaalswns321@naver.com>
2021-04-16 18:54:00 +0900

    feat: camo start

--- algorithm/programmers/level2_camo.js
+++ algorithm/programmers/level2_camo.js
@@ -1,1 +1,26 @@
 //refer : https://programmers.co.kr/learn/courses/30/lessons/42578?language=javascript
+
+const test = {
+  input: [
+    ["yellowhat", "headgear"],
+    ["bluesunglasses", "eyewear"],
+    ["green_turban", "headgear"],
+  ],
+  output: 5,
+};
+
+function solution(clothes) {
+  var answer = 0;
+  const clotheList = {};
+  for (let i = 0; i < clothes.length; i++) {
+    const clothe = clothes[i];
+    if (!clotheList[clothe[1]]) {
+      clotheList[clothe[1]] = [];
+    }
+    clotheList[clothe[1]].push(clothe[0]);
+  }
+  console.log(clotheList);
+  return answer;
+}
+
+console.log(solution(test.input));
