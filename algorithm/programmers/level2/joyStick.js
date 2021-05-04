3ba4531f2913036a6c0606502185e7fd23aba4a8
DESKTOP-OL74JA4\rlaal <rlaalswns321@naver.com>
2021-05-04 16:59:41 +0900

    feat: joyStick start

---
+++ algorithm/programmers/level2/joyStick.js
@@ -0,0 +1,21 @@
+//refer : https://programmers.co.kr/learn/courses/30/lessons/42860?language=javascript
+
+const test = {
+    1: {
+        input : "JEROEN",
+        output : 56
+    },
+    2: {
+        input : "JAN",
+        output : 23
+    }
+}
+
+
+function solution(name) {
+    var answer = 0;
+    return answer;
+}
+
+const testCase = test["1"].input
+console.log(solution(testCase));
