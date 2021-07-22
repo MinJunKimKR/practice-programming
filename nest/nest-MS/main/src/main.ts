3d8e59eb77974eefb72bc33a37d4373e85fb3a4f
minjun.kim <rlaalswns321@naver.com>
2021-07-22 20:08:28 +0900

    feat: 25:32 done main app is made

--- nest/nest-MS/main/src/main.ts
+++ nest/nest-MS/main/src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  //api/product ...
   app.enableCors({
     origin: 'http://localhost:4200',
   });
-  await app.listen(8000);
+  await app.listen(8001);
 }
 bootstrap();
