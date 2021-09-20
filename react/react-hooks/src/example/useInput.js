81d9d3d17d9c56243631b02bb8fe4a9d1971dba0
minjun.kim <rlaalswns321@naver.com>
2021-09-20 17:19:14 +0900

    feat: finish 1.3

--- react/react-hooks/src/example/useInput.js
+++ react/react-hooks/src/example/useInput.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validatior) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validatior === "function") {
+      willUpdate = validatior(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr. ");
+  const maxLen = (value) => value.includes("@");
+  const name = useInput("Mr. ", maxLen);
   return (
     <div className="App">
       <h2>Start editing to see some magic happen!</h2>
